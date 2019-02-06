43fbc9427118dff699ad8f13a288e649f23c89da
Vladimir Efimenko <vladimir.efimenko@gmail.com>
2019-02-06 23:48:09 +0200

    UI

--- OmdbSearchUI/wwwroot/js/App.js
+++ OmdbSearchUI/wwwroot/js/App.js
@@ -1,34 +1,52 @@
-﻿let AppSettings = {
+﻿var AppSettings = {
     apiBaseUrl: "api/"
 }
 
-let app = new Vue({
+var app = new Vue({
     el: "#app",
     data: {
         searchTerm: "",
+        totalResults: 0,
         currentPage: 1,
-        totalPages: 0,
-        errorMessage: null
+        PageSize: 10,
+        errorMessage: null,
+        searchResult: null
     },
     methods: {
-        search: function() {
+        search: function () {
+            this.cleanData();
             console.info("Searching for " + this.searchTerm);
-            let self = this;
+            var self = this;
             $.ajax({
                 url: AppSettings.apiBaseUrl + "search",
                 data: { title: this.searchTerm, page: this.currentPage },
                 success: function (data) {
                     if (data.error && data.error !== "") {
-                        self.errorMessage = data.error;
-                        $("alert").show();
+                        self.showAlert(data.error);
+                    } else {
+                        self.searchResult = data.search;
+                        self.totalResults = data.totalResults;
                     }
-
+                },
+                error: function(error) {
+                    self.showAlert(error);
                 }
         });
+        },
+        cleanData: function() {
+            this.errorMessage = null;
+            this.searchResult = null;
+        },
+        showAlert: function(error) {
+            this.errorMessage = error;
+            $("alert").show();
         },
         closeAlert: function() {
             $("alert").hide();
             this.errorMessage = null;
+        },
+        showMovie: function (imdbId) {
+            console.info("Show movie " + imdbId);
         }
     }
 });
